aaa3a21aa5b013443b10fc8396e7f672f53455c7
VTaran <vadzim.taran@gmail.com>
2021-05-20 19:05:58 +0300

    push and pop for stack

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -3,6 +3,8 @@ My little Stack
 """
 from typing import Any
 
+# stack variable
+my_stack = []
 
 def push(elem: Any) -> None:
     """
@@ -10,8 +12,13 @@ def push(elem: Any) -> None:
 
     :param elem: element to be pushed
     :return: Nothing
+
+    O(1)
     """
     print(elem)
+
+    my_stack.append(elem)
+
     return None
 
 
@@ -20,8 +27,14 @@ def pop() -> Any:
     Pop element from the top of the stack. If not elements - should return None.
 
     :return: popped element
+
+     O(1)
     """
-    return None
+
+    popped_elem = None if not my_stack else my_stack[len(my_stack)-1]
+    my_stack.pop()
+
+    return popped_elem
 
 
 def peek(ind: int = 0) -> Any:
@@ -30,6 +43,8 @@ def peek(ind: int = 0) -> Any:
 
     :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
     :return: peeked element or None if no element in this place
+
+     O()
     """
     print(ind)
     return None
@@ -40,5 +55,17 @@ def clear() -> None:
     Clear my stack
 
     :return: None
+
+     O()
     """
     return None
+
+
+if __name__ == '__main__':
+    for i in range(10):
+        push(i)
+    print(my_stack)
+
+    for _ in range(10):
+        pop()
+    print(my_stack)
