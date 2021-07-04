b3202df2a6b6038a93f64f80c9b45d5f90124fb9
VTaran <vadzim.taran@gmail.com>
2021-07-04 15:20:42 +0300

    while: Tests are OK

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,7 +2,9 @@
 Taylor series
 """
 from typing import Union
+from math import factorial
 
+DELTA = 0.0001
 
 def ex(x: Union[int, float]) -> float:
     """
@@ -22,5 +24,27 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
+
     print(x)
-    return 0
+
+    def _get_item(n):
+        # only more than 1
+        if not n >= 1:
+            raise ValueError
+
+        # only int
+        if not isinstance(n, int):
+            raise TypeError
+
+        return ((-1) ** (n + 1)) * (x ** (2 * n - 1)) / factorial(2 * n - 1)
+
+    res = 0
+    n = 1 # Todo itertools: count
+    while True:
+        current_elem = _get_item(n)
+        if abs(current_elem) < DELTA:
+            break
+        res += current_elem
+        n += 1
+
+    return res
