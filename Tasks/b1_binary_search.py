78828a20d7e4468ae56fe8df189bfc189c8ad472
VTaran <vadzim.taran@gmail.com>
2021-07-04 16:23:53 +0300

    Tests are OK

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -10,4 +10,20 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :return: Index of element if it's presented in the arr, None otherwise
     """
     print(elem, arr)
+
+    start = 0
+    end = len(arr) - 1
+    mid = 0
+
+    while start <= end:
+        mid = start // 2 + end // 2
+
+        if arr[mid] == elem:
+            return mid
+        elif arr[mid] > elem:
+            end = mid - 1
+        else:
+            start = mid + 1
+
     return None
+
