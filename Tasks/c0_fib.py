562db8d61e047480034ffb18212f4ffe08843c5e
VTaran <vadzim.taran@gmail.com>
2021-07-05 18:23:49 +0300

    Tests are OK

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 def fib_recursive(n: int) -> int:
     """
     Calculate n-th number of Fibonacci sequence using recursive algorithm
@@ -6,7 +8,17 @@ def fib_recursive(n: int) -> int:
     :return: Fibonacci number
     """
     print(n)
-    return 0
+
+    if n < 0:
+        raise ValueError
+
+    if n == 0:
+        return 0
+
+    if n == 1:
+        return 1
+
+    return fib_recursive(n-2) + fib_recursive(n-1)
 
 
 def fib_iterative(n: int) -> int:
@@ -17,4 +29,21 @@ def fib_iterative(n: int) -> int:
     :return: Fibonacci number
     """
     print(n)
-    return 0
+
+    if n < 0:
+        raise ValueError
+
+    if n == 0:
+        return 0
+
+    if n == 1:
+        return 1
+
+    res = [0 for _ in range(n+1)]
+
+    res[1] = 1
+
+    for i in range(2, n+1):
+        res[i] = res[i-1] + res[i-2]
+
+    return res[n]
