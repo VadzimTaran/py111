17e52ecbfee6099688a85dce0e17034655cd2ded
VTaran <vadzim.taran@gmail.com>
2021-07-07 23:13:07 +0300

    All Tests are OK

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -11,4 +11,21 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :return: list of nodes in the visited order
     """
     print(g, start_node)
-    return list(g.nodes)
+
+    # visited nodes
+    visited = []
+
+    # special routine
+    def dfsRoutine(visited_lst: list, graph: nx.Graph, node: Hashable):
+        # Mark the current node as visited
+        visited_lst.append(node)
+        # For all neighbours
+        for neighbour in graph[node]:
+            # node is not processed yet
+            if neighbour not in visited_lst:
+                # process
+                dfsRoutine(visited_lst, graph, neighbour)
+
+    dfsRoutine(visited, g, start_node)
+
+    return visited
