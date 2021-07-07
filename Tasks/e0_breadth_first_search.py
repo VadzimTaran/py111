5fba5760a1babb0252cb854855d9eb5afbe8674d
VTaran <vadzim.taran@gmail.com>
2021-07-07 21:58:06 +0300

    All Tests are OK

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -10,5 +10,26 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+
+    # queue for processing nodes; put the 1st element
+    queue = [start_node]
+
+    # queue for visited nodes; put the 1st element
+    visited = [start_node]
+
+    # check the queue, until not empty
+    while queue:
+        # take 1st elem from queue and remove it
+        procd_elem = queue.pop(0)
+        # check neighbours
+        for neighbour in g[procd_elem]:
+            # visited?
+            if neighbour not in visited:
+                # no
+                # add it into visited
+                visited.append(neighbour)
+                # add it into queue for next processing
+                queue.append(neighbour)
+
+    return visited
+
