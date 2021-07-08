9f27e5b690cfc9b6d06d428709dc18e822c85c0d
VTaran <vadzim.taran@gmail.com>
2021-07-08 11:35:04 +0300

    Improvement - check if elements swapped

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -12,11 +12,18 @@ def sort(container: List[int]) -> List[int]:
 
     # go through the whole list
     for i in range(0, len(container) - 1):
+        # improvement: check swapping
+        elem_swapped = False
         # range can be shorten by i because max value is shifted to the most right place
         for j in range(len(container) - i - 1):
             if container[j] > container[j + 1]:
                 # swap in python's way
                 container[j], container[j + 1] = container[j + 1], container[j]
+                # set swapped to True
+                elem_swapped = True
+        # if no elements swapped - container is sorted; no need to go through it any more
+        if not elem_swapped:
+            break
 
     #print(container)
 
