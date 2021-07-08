d315ceb6eff392c3ecea59faf6ed115dd4eb8970
VTaran <vadzim.taran@gmail.com>
2021-07-08 16:16:58 +0300

    Alternative implementation - not yet done

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -12,11 +12,29 @@ def sort(container: List[int]) -> List[int]:
     if len(container) <= 1:
         return container
 
-    pivot_idx = len(container) // 2
-    pivot = container[pivot_idx]
+    def partition(list_, start_, end_):
+        pivot_idx = len(container) // 2
+        pivot = list_[pivot_idx]
+        left = start_ + 1 if pivot_idx == start_ else start_
+        right = end_
+        done = False
+        while not done:
+            while left <= right and list_[left] <= pivot:
+                left = left + 1
+            while list_[right] >= pivot and right >= left:
+                right = right - 1
+            if right < left:
+                done = True
+            else:
+                list_[left], list_[right] = list_[right], list_[left]
+        list_[start_], list_[right] = list_[right], list_[start_]
+        return right
 
-    less_part = [less_val for less_val in container if less_val < pivot]
-    equal_part = [equal_val for equal_val in container if equal_val == pivot]
-    more_part = [more_val for more_val in container if more_val > pivot]
+    def quickSort(list_, start_, end_):
+        if start_ < end_:
+            pivot = partition(list_, start_, end_)
+            quickSort(list_, start_, pivot - 1)
+            quickSort(list_, pivot + 1, end_)
+        return list_
 
-    return sort(less_part) + equal_part + sort(more_part)
+    return quickSort(container, 0, len(container)-1)
