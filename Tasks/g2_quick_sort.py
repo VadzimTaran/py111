8cfe0e14e4d3b366ed4b8b1a629b154ccb584ad6
VTaran <vadzim.taran@gmail.com>
2021-07-08 13:50:27 +0300

    All tests are done: memory consuming implementation

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -8,4 +8,15 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+
+    if len(container) <= 1:
+        return container
+
+    pivot_idx = len(container) // 2
+    pivot = container[pivot_idx]
+
+    less_part = [less_val for less_val in container if less_val < pivot]
+    equal_part = [equal_val for equal_val in container if equal_val == pivot]
+    more_part = [more_val for more_val in container if more_val > pivot]
+
+    return sort(less_part) + equal_part + sort(more_part)
