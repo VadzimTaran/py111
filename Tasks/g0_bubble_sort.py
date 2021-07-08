254148dd16f3a00b78920b9615db59beb7d849ae
VTaran <vadzim.taran@gmail.com>
2021-07-08 11:13:19 +0300

    All Tests are OK

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -8,4 +8,16 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    #print(container)
+
+    # go through the whole list
+    for _ in range(0, len(container) - 1):
+        for j in range(len(container) - 1):
+            if container[j] > container[j + 1]:
+                temp = container[j]
+                container[j] = container[j + 1]
+                container[j + 1] = temp
+
+    #print(container)
+
     return container
